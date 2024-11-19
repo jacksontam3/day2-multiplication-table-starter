c17529747df9e4e8f74e226c859c0ad66545e70b
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-19 10:35:08 +0800

    [MultiplicationTable] test, should return false when isInRange given a number 1001

--- src/test/java/com/tw/MultiplicationTableBuilderTest.java
+++ src/test/java/com/tw/MultiplicationTableBuilderTest.java
@@ -32,6 +32,19 @@ class MultiplicationTableBuilderTest {
         assertFalse(isInRange);
     }
 
+    @Test
+    void should_return_false_when_isInRange_given_a_number_1001() {
+        // Given
+        MultiplicationTableBuilder multiplicationTableBuilder = new MultiplicationTableBuilder();
+        int number = 1001;
+
+        // When
+        boolean isInRange = multiplicationTableBuilder.isInRange(number);
+
+        // Then
+        assertFalse(isInRange);
+    }
+
 
 
     // TODO: add more tests for isInRange
