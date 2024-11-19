34690ece4fc309690ebac57335c3c6d07670a38c
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-19 11:25:05 +0800

    [MultiplicationTable] test, should return true when isValid given start 2 and end 3

--- src/test/java/com/tw/MultiplicationTableBuilderTest.java
+++ src/test/java/com/tw/MultiplicationTableBuilderTest.java
@@ -87,6 +87,21 @@ class MultiplicationTableBuilderTest {
         assertFalse(isStarterNotBiggerThanEnd);
     }
 
+    @Test
+    void should_return_true_when_isValid_given_start_2_and_end_3() {
+        // Given
+        MultiplicationTableBuilder multiplicationTableBuilder = new MultiplicationTableBuilder();
+        int start = 2;
+        int end = 3;
+
+        // When
+        boolean isValid = multiplicationTableBuilder.isValid(start, end);
+
+        // Then
+        assertTrue(isValid);
+    }
+
+
 
 
     // TODO: add more tests for isInRange
