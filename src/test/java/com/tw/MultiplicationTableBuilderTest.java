9ee653525f105a195de8ef478f1bf416a017c3fd
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-19 11:31:20 +0800

    [MultiplicationTable] test, should return false when isValid given start 2 and end 1001

--- src/test/java/com/tw/MultiplicationTableBuilderTest.java
+++ src/test/java/com/tw/MultiplicationTableBuilderTest.java
@@ -129,6 +129,20 @@ class MultiplicationTableBuilderTest {
         assertFalse(isValid);
     }
 
+    @Test
+    void should_return_false_when_isValid_given_start_2_and_end_1001(){
+        // Given
+        MultiplicationTableBuilder multiplicationTableBuilder = new MultiplicationTableBuilder();
+        int start = 2;
+        int end = 1001;
+
+        // When
+        boolean isValid = multiplicationTableBuilder.isValid(start, end);
+
+        // Then
+        assertFalse(isValid);
+    }
+
 
 
 
